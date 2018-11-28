bded7ad4be945220f50fd1b2befa3aa33d347d0b
Parthiv Mohan <parthiv.mohan@gmail.com>
2018-11-28 14:15:57 -0500

    prior to blog conversion

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :delete_all
 	has_many :taggings, dependent: :delete_all
-	has_many :tags, through: :taggings
+	has_one :author
+	has_many :tags, through: :taggings, dependent: :destroy
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
